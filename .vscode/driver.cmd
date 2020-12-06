034a6f0d9536f45aaa6d626dba94e5af9a6e18c8
dbjdbj <dbj@dbj.org>
2020-12-06 11:46:46 +0100

    so far so good, not done yet

--- .vscode/driver.cmd
+++ .vscode/driver.cmd
@@ -2,12 +2,16 @@
 
 @setlocal
 
+@rem ~ n -- remove quotes arround param n
+
 @echo 1 = [%1]
-@echo 2 = [%2]
+@echo 2 = [%~2]
 @echo 3 = [%3]
 @echo 4 = [%4]
 @echo 5 = [%5]
 @echo 6 = [%6]
 @echo 7 = [%7]
 @echo 8 = [%8]
+
+%1 %~2 %~3
 
